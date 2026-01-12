d47157ae3999a3045f3a469fae90086e22862cc4
Abhijeet Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-01-12 20:23:40 +0530

    ➕ Added basic example of creating OS threads
    1️⃣Added basic example of creating OS threads

--- creating-threads/src/main.rs
+++ creating-threads/src/main.rs
@@ -1,3 +1,21 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let v: Vec<i32> = vec![1, 2, 3, 4, 5];
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+    handle.join().unwrap();
+    let handler = thread::spawn(|| {
+        for i in 1..10 {
+            println!("Hi from spawned thread: {i}");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    for i in 1..5 {
+        println!("Hi from main thread: {i}");
+        thread::sleep(Duration::from_millis(1));
+    }
+    handler.join().unwrap();
 }
